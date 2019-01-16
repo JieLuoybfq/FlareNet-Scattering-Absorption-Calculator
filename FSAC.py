e0b221d8c1c51d284c8533b2521114f4ada8213c
Keyhan <37202779+keyhanB@users.noreply.github.com>
2019-01-16 04:08:48 -0800

    Reformatting Scattering Functions
    Signed-off-by: Keyhan <37202779+keyhanB@users.noreply.github.com>

--- FSAC.py
+++ FSAC.py
@@ -32,19 +32,20 @@ if __name__ == "__main__":
     ####################### Particle Sample Setting
 
     Sample_Total_Number_Concentration = 1  # /cm^3
-    Sample_LogN_D_Median_Min = 350  # Smallest diameter of computation (nm)
-    Sample_LogN_D_Median_Max = 800  # Largest diameter of computation (nm)
+    Sample_LogN_D_Median_Min = 200  # Smallest diameter of computation (nm)
+    Sample_LogN_D_Median_Max = 550  # Largest diameter of computation (nm)
     Sample_LogN_D_Median_Bins = 2  # Number of the Steps
     Sample_LogN_Sigma_Min = 1  # Smallest Sigma G
-    Sample_LogN_Sigma_Max = 2.5  # Largest Sigma G
+    Sample_LogN_Sigma_Max = 1.7  # Largest Sigma G
     Sample_LogN_Sigma_Bins = 2  # Number of the Steps
     Sample_Sigma_Bound = 2.5  # Number of Sigma G to cover
     Sample_Sigma_Bins = 300  # Number of bins
 
     ####################### Varation of Primary Particle Size within the sample
 
-    Primary_Diameter_100nm = 16.48 * 1e-9  # Primary particle diameter of 100 nm aggregate
-    Primary_D_TEM = 0.45
+    # Primary_Diameter_100nm = 16.48 * 1e-9  # Primary particle diameter of 100 nm aggregate
+    # Primary_D_TEM = 0.45
+    Primary_D_TEM = 0
     Primary_Sigma_da_CTE = 1  # Sigma G around specific particle primary particle size
     Primary_Sigma_da_CTE_Bound = 2.5  # Number of Sigma G to cover
     Primary_Sigma_da_CTE_Nt = 19 + 1  # Number of bins
@@ -52,7 +53,7 @@ if __name__ == "__main__":
 
     ####################### Refractive Index
 
-    Soot_Refractive_Index = 1.95 - 0.79j  # Complex refractive index
+    Soot_Refractive_Index = 1.6 - 0.6j  # Complex refractive index
     Soot_Primary_Agg_dp_sigma = 1  # Geometric std of primary particles within aggregate
 
     ####################### Continuum regime
@@ -64,10 +65,10 @@ if __name__ == "__main__":
 
     ####################### Free molecular regime
 
-    Soot_Fractal_D_mfm = 0.46  # Fractal Properties free molecular regime
-    Soot_Prefactor_k_mfm = 0.94 + (0.03 * Soot_Primary_Agg_dp_sigma ** (4.8))  # Fractal free molecular regime
-    Soot_Fractal_D_mfm_RDG = 1 / Soot_Fractal_D_mfm
-    Soot_Prefactor_k_mfm_RDG = (Primary_Betha / Soot_Prefactor_k_mfm) ** (1 / Soot_Fractal_D_mfm)
+    # Soot_Fractal_D_mfm = 0.46  # Fractal Properties free molecular regime
+    # Soot_Prefactor_k_mfm = 0.94 + (0.03 * Soot_Primary_Agg_dp_sigma ** (4.8))  # Fractal free molecular regime
+    # Soot_Fractal_D_mfm_RDG = 1 / Soot_Fractal_D_mfm
+    # Soot_Prefactor_k_mfm_RDG = (Primary_Betha / Soot_Prefactor_k_mfm) ** (1 / Soot_Fractal_D_mfm)
 
     ####################### Effective Density
 
@@ -76,9 +77,9 @@ if __name__ == "__main__":
 
     ####################### Wave Parameter
 
-    Wave_Length = 550 * 1e-9  # nm
+    Wave_Length = 1064 * 1e-9  # nm
     Wave_Number = 2 * pi / Wave_Length  # k
-
+    Primary_Diameter_100nm = 0.0886 * Wave_Length / pi
     ####################### Scattering Parameters
 
     Theta_Number = 200
@@ -103,21 +104,45 @@ if __name__ == "__main__":
 
     #######################
 
-    # Diameter_Meter = []
-    # Diameter_Log = []
-    # Diameter_Nano = []
-    # Bound_D_Max = 1 * 10 ** (-6)
-    # Bound_D_Min = 10 * 10 ** (-9)
-    # Bound_D_Bins = 300
-    # Keyhan = 10 ** 9  # Conversion factor
-    # D_ratio = (Bound_D_Max / Bound_D_Min) ** (1 / (Bound_D_Bins - 1))  # Generating particle diameters
-    # for i in range(Bound_D_Bins):
-    #     d = Bound_D_Min * D_ratio ** (i)
-    #     Diameter_Meter.append(d)
-    #     Diameter_Log.append(log(d))
-    #     Diameter_Nano.append(d * Keyhan)
+    Diameter_Meter = []
+    Diameter_Log = []
+    Diameter_Nano = []
+    Bound_D_Max = 1000 * 10 ** (-9)
+    Bound_D_Min = 30 * 10 ** (-9)
+    Bound_D_Bins = 300
+    Keyhan = 10 ** 9  # Conversion factor
+    D_ratio = (Bound_D_Max / Bound_D_Min) ** (1 / (Bound_D_Bins - 1))  # Generating particle diameters
+    for i in range(Bound_D_Bins):
+        d = Bound_D_Min * D_ratio ** (i)
+        Diameter_Meter.append(d)
+        Diameter_Log.append(log(d))
+        Diameter_Nano.append(d * Keyhan)
 
     #######################
+
+    # Test
+    Test_Np = [5, 10, 20, 50, 100, 150, 199, 348, 546, 893]
+    Test_Wave1 = 532 * 10 ** (-9)
+    Test_Wave2 = 1064 * 10 ** (-9)
+    Test_Wave_Number1 = 2 * pi / Test_Wave1  # k
+    Test_Wave_Number2 = 2 * pi / Test_Wave2  # k
+    Test_Primary_Diameter1 = 0.177 * Test_Wave1 / pi
+    Test_Primary_Diameter2 = 0.0886 * Test_Wave2 / pi
+    Test_Soot_Prefactor_k_mc_RDG = 2.3
+    Test_Soot_Fractal_D_mc_RDG = 1.78
+    Test_Soot_Refractive_Index = 1.6 - 0.6j
+    Test_Soot_Complex = (((Test_Soot_Refractive_Index ** 2) - 1) / ((Test_Soot_Refractive_Index ** 2) + 2))
+    Test_Soot_FM = (abs(Test_Soot_Complex)) ** 2
+    Test_Soot_EM = Test_Soot_Complex.imag
+    Test_Absorption_Cross_Section_Agg1 = []
+    Test_Absorption_Cross_Section_Agg2 = []
+    Test_Scattering_Cross_Section_Agg1 = []
+    Test_Scattering_Cross_Section_Agg2 = []
+    for i in range(len(Test_Np)):
+        Test_Absorption_Cross_Section_Agg1.append((10 ** 18) * FN.RDG_Absorption(K=Test_Wave_Number1, N=Test_Np[i], Dp=Test_Primary_Diameter1, E=Test_Soot_EM))
+        Test_Absorption_Cross_Section_Agg2.append((10 ** 18) * FN.RDG_Absorption(K=Test_Wave_Number2, N=Test_Np[i], Dp=Test_Primary_Diameter2, E=Test_Soot_EM))
+        Test_Scattering_Cross_Section_Agg1.append((10 ** 18) * FN.RDG_Total_Scattering(K=Test_Wave_Number1, N=Test_Np[i], Dp=Test_Primary_Diameter1, F=Test_Soot_FM, D_RDG=Test_Soot_Fractal_D_mc_RDG, K_RDG=Test_Soot_Prefactor_k_mc_RDG, Formula=2))
+        Test_Scattering_Cross_Section_Agg2.append((10 ** 18) * FN.RDG_Total_Scattering(K=Test_Wave_Number2, N=Test_Np[i], Dp=Test_Primary_Diameter2, F=Test_Soot_FM, D_RDG=Test_Soot_Fractal_D_mc_RDG, K_RDG=Test_Soot_Prefactor_k_mc_RDG, Formula=2))
 
     # Total_Absorption_Cross = [[0 for j in range(LogNormal_Sigma_Number_Sample)] for i in range(LogNormal_D_Median_Number_Sample)]
     # Scattering_Cross = [[[0 for k in range(Nd)] for j in range(LogNormal_Sigma_Number_Sample)] for i in range(LogNormal_D_Median_Number_Sample)]
@@ -135,11 +160,11 @@ if __name__ == "__main__":
     Y_Label4 = "Absorption Efficiency"
     Y_Label5 = "Scattering Cross Section(m^2)"
     Y_Label6 = "Sample Mass(kg)"
-
+    #
     for i in range(Sample_LogN_D_Median_Bins):
         for j in range(Sample_LogN_Sigma_Bins):
 
-            Diameter_Meter, Diameter_Log, Diameter_Nano = FN.Bins_LogN_Distributed(Median_Diameter=Sample_D_Median_List[i] * 10 ** (-9), Sigma_G=Sample_Sigma_List[j], Sigma_G_Bound=Sample_Sigma_Bound, Total_Number_Bins=Sample_Sigma_Bins)
+            # Diameter_Meter, Diameter_Log, Diameter_Nano = FN.Bins_LogN_Distributed(Median_Diameter=Sample_D_Median_List[i] * 10 ** (-9), Sigma_G=Sample_Sigma_List[j], Sigma_G_Bound=Sample_Sigma_Bound, Total_Number_Bins=Sample_Sigma_Bins)
             LogN_Sample_SizeDistribution_Plain = []
             LogN_Sample_SizeDistribution = []
 
@@ -238,7 +263,7 @@ if __name__ == "__main__":
 
                 Scattering_Cross_Section_Total_Specific_Particle_Size = 0
                 for p in range(Primary_Sigma_da_CTE_Nt - 1):
-                    Scattering_Cross_Section_Total_Agg = FN.RDG_Total_Scattering(K=Wave_Number, N=Primary_Number_Bank[k][p], Dp=Primary_Diameter_Bank[k][p], F=Soot_FM, D_RDG=Soot_Fractal_D_mc_RDG, K_RDG=Soot_Prefactor_k_mc_RDG, check=Primary_Probability_Bank[k][p], Number=k)
+                    Scattering_Cross_Section_Total_Agg = FN.RDG_Total_Scattering(K=Wave_Number, N=Primary_Number_Bank[k][p], Dp=Primary_Diameter_Bank[k][p], F=Soot_FM, D_RDG=Soot_Fractal_D_mc_RDG, K_RDG=Soot_Prefactor_k_mc_RDG)
                     Scattering_Cross_Section_Total_Specific_Particle_Size += Scattering_Cross_Section_Total_Agg * Primary_Probability_Bank[k][p]
 
                 Scattering_Cross_Section_Total_Distribution.append(Scattering_Cross_Section_Total_Specific_Particle_Size)
@@ -248,14 +273,16 @@ if __name__ == "__main__":
             FN.Fig_Plot_Save_1Lines_X_Log_Y_Linear(Address, Diameter_Nano[:-1], X_Label1, Scattering_Cross_Section_Total_Distribution, "Scattering Cross Section (Total)=\n" + str(Scattering_Cross_Section_Total), Y_Label5, Situation)
 
             # MAC_MSC
-            Mass_Sample = []
+            Mass_Sample_Eff = []
             Density = []
-            Total_Mass = 0
+            Total_Mass_Eff = 0
             for k in range(Sample_Sigma_Bins - 1):
                 Density.append(FN.Effective_Density(K=Eff_k, Dm=Eff_dm, da=Diameter_Meter[k]))
-                Mass_Sample.append(FN.Mass_Calc(rho=Density[k], da=Diameter_Meter[k]))
-                Total_Mass += Mass_Sample[k] * LogN_Sample_SizeDistribution[k]
-            MAC = Absorption_Cross_Section / Total_Mass
-            MSC = Scattering_Cross_Section_Total / Total_Mass
+                Mass_Sample_Eff.append(FN.Mass_Calc(rho=Density[k], da=Diameter_Meter[k]))
+                Total_Mass_Eff += Mass_Sample_Eff[k] * LogN_Sample_SizeDistribution[k]
+            Total_Mass_Eff = Total_Mass_Eff * 1000  # kg to g
+            MAC = Absorption_Cross_Section / Total_Mass_Eff
+            MSC = Scattering_Cross_Section_Total / Total_Mass_Eff
             Address = FN.File_Pointer(Main=script_dir, FolderName=Graph_Folder, FileName="_Mass" + "-" + Situation, Extension="jpg")
-            FN.Fig_Plot_Save_1Lines_X_Log_Y_Linear(Address, Diameter_Nano[:-1], X_Label1, Mass_Sample, "MAC=" + str(MAC) + "\n" + "MSC=" + str(MSC), Y_Label5, Situation)
+            FN.Fig_Plot_Save_1Lines_X_Log_Y_Linear(Address, Diameter_Nano[:-1], X_Label1, Mass_Sample_Eff, "MAC=" + str(MAC) + "\n" + "MSC=" + str(MSC), Y_Label6, Situation)
+            a = 3
--- Functions.py
+++ Functions.py
@@ -130,16 +130,15 @@ def LogN_Distribution(Median, SigmaG, Dp2, Dp1):  # return number between 0 to 1
         raise
 
 
-def RDG_Def_Scattering(K, N, Dp, q, F, D_RDG, K_RDG, check, Number):
+def RDG_Def_Scattering(K, N, Dp, q, F, D_RDG, K_RDG, Formula=1):
     try:
 
         Monomer_Differential = F * ((Dp / 2) ** 6) * (K ** 4)
         Rg = ((N / K_RDG) ** (1 / D_RDG)) * (Dp / 2)
-        S_q = RDG_Structure_Factor(q=q, Rg=Rg, D_FD=D_RDG)
+        S_q = RDG_Structure_Factor(q=q, Rg=Rg, D_RDG=D_RDG, K_RDG=K_RDG, Formula=Formula)
         Aggregate_Differential = (N ** 2) * Monomer_Differential * S_q
-        if check == 1:
-            if Number == 194:
-                logging.debug(f"RDG_Def_Scattering---Number={Number},Monomer_Differential={Monomer_Differential},Rg={Rg},S_q={S_q},Aggregate_Differential={Aggregate_Differential}")
+
+        logging.debug(f"RDG_Def_Scattering,Monomer_Differential={Monomer_Differential},Rg={Rg},S_q={S_q},Aggregate_Differential={Aggregate_Differential}")
         return Aggregate_Differential
 
     except Exception as e:
@@ -158,14 +157,18 @@ def Scattering_Wave_Vector(WaveLength_meter, Theta_radian):
         raise
 
 
-def RDG_Structure_Factor(q, Rg, D_FD, C=1):
+def RDG_Structure_Factor(q, Rg, D_RDG, K_RDG, Formula=1, C=1):
     try:
+        if Formula == 1:  # Sorensen 2001
+            C = 1.35 / K_RDG
+            Check = q * Rg
+            if Check <= 1:
+                A = 1 - ((Check ** 2) / 3)  # Guinier equation (Guinier 1939;Guinier et al. 1955; Teixeira 1986).
+            elif Check > 1:
+                A = C * (Check) ** (-1 * D_RDG)
+        elif Formula == 2:  # Yang 2005
+            A = (1 + ((8 * (q * Rg) ** 2) / (3 * D_RDG)) + (q * Rg) ** 8) ** (-1 * D_RDG / 8)
 
-        Check = q * Rg
-        if Check <= 1:
-            A = 1 - ((Check ** 2) / 3)  # Guinier equation (Guinier 1939;Guinier et al. 1955; Teixeira 1986).
-        elif Check > 1:
-            A = C * (Check) ** (-1 * D_FD)
         return A
 
     except Exception as e:
@@ -198,15 +201,27 @@ def RDG_Absorption(K, N, Dp, E):
         raise
 
 
-def RDG_Total_Scattering(K, N, Dp, F, D_RDG, K_RDG, check, Number):
+def RDG_Total_Scattering(K, N, Dp, F, D_RDG, K_RDG, Formula=1):
     try:
+        if Formula == 1:  # Sorensen 2001
+            Monomer_Total = (8 / 3) * pi * (K ** 4) * ((Dp / 2) ** 6) * F
+            Rg = ((N / K_RDG) ** (1 / D_RDG)) * (Dp / 2)
+            G = (1 + ((4 / (3 * D_RDG)) * ((K * Rg) ** 2))) ** (-0.5 * D_RDG)
 
-        Monomer_Total = (8 / 3) * pi * (K ** 4) * ((Dp / 2) ** 6) * F
-        Rg = ((N / K_RDG) ** (1 / D_RDG)) * (Dp / 2)
-        G = (1 + ((4 / (3 * D_RDG)) * ((K * Rg) ** 2))) ** (-0.5 * D_RDG)
-        Aggregate_Total = (N ** 2) * Monomer_Total * G
-        if check == 1:
-            logging.debug(f"RDG_Total_Scattering---Number={Number},Monomer_Total={Monomer_Total},Rg={Rg},G={G},Aggregate_Total={Aggregate_Total}")
+            Aggregate_Total = (N ** 2) * Monomer_Total * G
+            logging.debug(f"RDG_Total_Scattering,Monomer_Total={Monomer_Total},Rg={Rg},G={G},Aggregate_Total={Aggregate_Total}_Sorensen(2001)")
+        if Formula == 2:  # Yang 2005
+            Monomer_Total = (8 / 3) * pi * (K ** 4) * ((Dp / 2) ** 6) * F
+            Rg = ((N / K_RDG) ** (1 / D_RDG)) * (Dp / 2)
+            Betha = 3 * D_RDG / (8 * (K * Rg) ** 2)
+            if Betha >= 1:
+                G = 1 - (2 * ((K * Rg) ** 2) / 3)
+            elif Betha < 1:
+                G = ((Betha / 2) * (3 - 3 * Betha + 2 * (Betha ** 2))) - ((((K * Rg * Betha) ** 2) / 3) * (3 - 4 * Betha + 3 * (Betha ** 2))) + (
+                        ((2 * K * Rg) ** (-1 * D_RDG)) * ((3 / (2 - D_RDG)) - (12 / ((6 - D_RDG) * (4 - D_RDG))) - (3 * (Betha ** (1 - D_RDG / 2)) * ((1 / (2 - D_RDG)) - (2 * Betha / (4 - D_RDG)) + (2 * (Betha ** 2) / (6 - D_RDG))))))
+
+            Aggregate_Total = (N ** 2) * Monomer_Total * G
+            logging.debug(f"RDG_Total_Scattering,Monomer_Total={Monomer_Total},Rg={Rg}, Betha={Betha},G={G},Aggregate_Total={Aggregate_Total}_Yang(2005)")
         return Aggregate_Total
 
     except Exception as e:
